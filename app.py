b42d3b93b2fee245c852d7b8f2923e6874c2ba38
Mujicash <andre27012001@gmail.com>
2023-05-28 13:57:46 -0500

    Departemento model - route

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from config import URI
 from utils.db import db
 from routes.propietario import propietario
 from routes.condominio import condominio
+from routes.departamento import departamento
 
 
 app = Flask(__name__)
@@ -12,6 +13,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 app.register_blueprint(propietario)
 app.register_blueprint(condominio)
+app.register_blueprint(departamento)
 
 
 db.init_app(app)
